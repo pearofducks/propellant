db9482d1b79c1f14ffc1ff4cb0e300c0f6308edc
Dave Honneffer <pearofducks@gmail.com>
2017-06-16 10:26:43 +0200

    Merge styleguide and refine baseline stuff

--- demo/App.vue
+++ demo/App.vue
@@ -10,8 +10,35 @@
       <button @click="showdialog" class="button">show a dialog</button>
     </p-modal>
     <DemoHeader v-once/>
-    <StyleGuide/>
     <main>
+      <color-palette></color-palette>
+      <section class="labelled" data-type="type">
+        <h1 class="header">Header level 1</h1>
+        <h2 class="header">Header level 2</h2>
+        <h3 class="header">Header level 3</h3>
+        <h4 class="header">Header level 4</h4>
+        <h5 class="header">Header level 5</h5>
+        <p>A paragraph with some <i>italics</i> and <b>bold</b> text.</p>
+      </section>
+      <section class="labelled" data-type="buttons">
+        <div class="input-group">
+          <a class="button">a.button</a>
+          <button class="button">normal</button>
+          <button disabled class="button">normal disabled</button>
+        </div>
+        <div class="input-group">
+          <button class="button button-primary">filled</button>
+          <button disabled class="button button-primary">filled disabled</button>
+        </div>
+        <div class="input-group">
+          <button class="button button-link">link style</button>
+          <button disabled class="button button-link">link style disabled</button>
+        </div>
+        <div class="input-group">
+          <button class="button button-primary loading">loading</button>
+          <button class="button loading">loading</button>
+        </div>
+      </section>
       <section class="labelled" data-type="inputs">
         <p-textarea :model.sync="inputModel" placeholder="a textarea" label="label for textarea" />
         <p-textarea disabled :model.sync="inputModel" placeholder="a textarea" label="label for disabled textarea" />
@@ -133,13 +160,14 @@
 </template>
 
 <script>
+import colorPalette from './ColorPalette.vue'
 import StyleGuide from './StyleGuide'
 import ModalContent from './ModalContent'
 import DemoHeader from './DemoHeader'
 
 export default {
   name: 'app',
-  components: { StyleGuide, ModalContent, DemoHeader },
+  components: { StyleGuide, ModalContent, DemoHeader, colorPalette },
   mounted() {
     this.$refs.looped[0].show()
   },
